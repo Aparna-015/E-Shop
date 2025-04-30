f634ff04e67bd3bffd5cb02470947c38deea697c
Aparna <aparnaraman995@gmail.com>
2025-04-29 13:26:02 -1200

    Added

---
+++ src/Constant/data.js
@@ -0,0 +1,71 @@
+const products=
+[
+
+    {
+      "id": 1,
+      "image": "assets/images/speaker-products.png",
+      "title": "Jaruati Store  Speaker",
+      "price":500,
+      "description": "Mini Wireless Bluetooth Speaker, Boost 4 Colourful, Electroplating Round Steel, Random Colour",
+     
+    },
+    {
+      "id": 2,
+      "image": "assets/images/gameplayer.png",
+      "title": "Game player",
+      "price":450,
+      "description": "Viart Creation 2025 New TV Gaming System | Plug & Play Video Game 4K Ultra HD Game Stick with 20,000+ Built-in Games.",
+    },
+    {
+      "id": 3,
+      "image": "assets/images/hp laptop.png",
+      "title": "Hp Laptop",
+      "price":21500,
+      "description": "13th Generation Intel® Core™ i5 processor .",
+    },
+    {
+        "id": 4,
+        "image": "assets/images/wireless-headphones.png",
+        "title": "wireless-headphone",
+        "price":2070,
+        "description": "Portronics Muffs M3 Over Ear Headphones with ANC Enabled, Upto 40 Hours Playtime, in-Built Mic, 40mm Driver, Powerful Bass, Soft Ear Cushion, BT 5.3V, AUX in, Foldable Design, Type C Fast Charging .",
+      },
+      {
+        "id": 5,
+        "image": "assets/images/canon-product.png",
+        "title": "Canon",
+        "price":35970,
+        "description": "Canon EOS 1500D DSLR Camera 1 Body, 18 - 55 mm Lens  Black"
+      },
+      {
+        "id": 6,
+        "image": "assets/Speaker-product.png",
+        "title": "Speaker",
+        "price":870,
+        "description": "ZEBRONICS Blues 2.0 Computer Speakers, 8 Watts, 45° Elevated Drivers, Dual Passive Radiators, USB Powered, AUX, Volume Control Knob, RGB LED, for Laptops & Desktop (Black)"
+ 
+      },
+      {
+        "id": 7,
+        "image": "assets/images/panasoni.png",
+        "title": "Panasonic LUMIX G7 Mirrorless Camera",
+        "price":28970,
+        "description": "Panasonic LUMIX G7 16.00 MP 4K Mirrorless Interchangeable Lens Camera Kit with 14-42 mm Lens (Black)"
+      },
+      {
+        "id": 8,
+        "image": "assets/images/gameplayer-product.png",
+        "title": "Panasonic LUMIX G7 Mirrorless Camera",
+        "price":870,
+        "description": "Panasonic LUMIX G7 16.00 MP 4K Mirrorless Interchangeable Lens Camera Kit with 14-42 mm Lens (Black)"
+      },
+      {
+        "id": 9,
+        "image": "assets/images/Lenovo-product.png",
+        "title": "Lenovo IdeaPad 3 12th Gen Intel Core i3-1215U Laptop",
+        "price":30770,
+        "description": "deaPad Slim 5 Gen 10, 33.78cms - AMD"
+      },
+      
+      
+  ]
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,34 +2,164 @@ import React from "react";
 import productImage from "../assets/Images/logo.png";
 import cart from "../assets/Images/cartt.png";
 import bgimg from "../assets/Images/Background.png";
+import speaker from "../assets/Images/speaker.png"
+import LAPTOP from "../assets/Images/LAPTOP.png";
+import DSLR from "../assets/Images/DSLR.png";
+import canon from "../assets/Images/canon.png"
+import symbol from "../assets/Images/Symbol.png"
+import icon from "../assets/Images/symbols.png"
+import devicon from "../assets/Images/deliver-icons.png";
+import crwnicon from "../assets/Images/crown-icon.png";
+import warrantyicon from "../assets/Images/Warranty.png"
 const Home = () => {
   return (
-    <div className="main-container">
-      <div className="main-section">
-        <div className="logo-section">
-          <img src={productImage} />
+    <div  className="container">
+      <div className="main-container">
+        <div className="main-section">
+          <div className="logo-section ">
+
+            <img src={productImage} alt=""/>
+          </div>
+
+          <div className="right-section">
+            <a href=""> Home</a>
+
+            <a href=""> About Us</a>
+
+            <a href=""> Signin</a>
+
+            <button className="button">Login</button>
+            <img src={cart} alt="no " />
+          </div>
+        </div>
+        <div className="hero-section">
+          <h1>Endless choices, just for you</h1>
+          <button>Explore Products</button>
         </div>
 
-        <div className="right-section">
-          <a href=""> Home</a>
-
-          <a href=""> About Us</a>
-
-          <a href=""> Signin</a>
-
-          <button className="button">Login</button>
-
-          <img src={cart} alt="no image" />
+        <div className="img-section">
+          <img src={bgimg} alt="" />
+        </div>
+        <div className="explore-section">
+        <h1>Explore Our Products</h1>
         </div>
       </div>
-      <div className="hero-section">
-        <h1>Endless choices, just for you</h1>
-        <button>Explore Products</button>
-      </div>
 
-      <div className="img-section">
-        <img src={bgimg}/>
-      </div>
+   <div className="head-section">
+    <h1>Handpicked collections just for you</h1>
+    </div> 
+       
+
+
+
+   <div className="product-container">
+
+    <div className="prdt-img1-section">
+
+
+    <img src={speaker} alt=""/>
+      <div className="prdt-head-section">
+
+  
+       
+        <h1>Speaker</h1>
+        <h1>(6 items)</h1>
+        </div>
+
+    </div>
+    <div className="prdt-img2-section">
+      
+      <img src={LAPTOP} alt=""/>
+      <div className="prdts-head-section">
+        <h1>Desktop & laptop</h1>
+        <h1>(6 items)</h1>
+        </div>
+    </div>
+    <div className="prdt-img3-section">
+        <img src={DSLR} alt=""/>
+        <div className="prdtss-head-section">
+        <h1>DSLR camera</h1>
+        <h1>(6 items)</h1>
+        </div>
+    </div>
+    <div className="prdt-img4-section">
+        <img src={LAPTOP} alt=""/>
+        <div className="prodt-head-section">
+        <h1>Laptop</h1> 
+        <h1>(6 items)</h1>
+        </div>
+    </div>
+
+   </div>
+
+
+
+
+
+
+
+<div className="image-view-container">
+  <div className="img-container">
+<img src={canon} alt=""/>
+  </div>
+  <div className="prdt-info-container"> 
+<h1>Canon camera</h1>
+<h2> We are committed to providing our customers with exceptional service, competitive
+pricing, and a wide range of.</h2>
+
+
+
+<div className="img-icon-section">
+  
+<img src={symbol} alt=""/>
+
+<h3>We are the UK’s largest provider, with more patrols in more places</h3>
+
+</div>
+<button>Shop Now</button>
+  </div>
+</div>
+
+
+
+
+
+
+{/* <div className="section">
+
+  <div className="dev-section">
+    <div><img src={devicon}/></div>
+    <div>
+
+      <h1>Free delivery</h1>
+      <h1>on order above $50,00</h1>
+    </div>
+
+  </div>
+
+  <div className="crt-section">
+    <div>
+    <img src={crwnicon}/>
+    </div>
+<div>
+  <h1>Best quality </h1>
+  <h1>best quality in low price</h1>
+</div>
+  </div>
+
+<div className="qlty-section">
+<div>
+  <img src={warrantyicon}/>
+</div>
+<div>
+  <h1>1 year warranty</h1>
+  <h1>Avaliable warranty</h1>
+</div>
+</div>
+</div> */}
+
+
+
     </div>
   );
 };
